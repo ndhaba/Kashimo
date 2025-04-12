fe46dc643db73428078f0354795a136f31598bc6
Nagaa Dhaba <30734056+ndhaba@users.noreply.github.com>
2025-04-11 22:26:16 -0400

    Separate event handler from Kashimo struct & add basic block update handler

--- src/kashimo.rs
+++ src/kashimo.rs
@@ -1,54 +1,49 @@
 use std::sync::Arc;
 
-use azalea::{Account, Client, Event};
-use tokio::{sync::Mutex, task::JoinHandle};
+use azalea::{protocol::packets::game::ClientboundGamePacket, Account, Client, Event};
+use tokio::{sync::mpsc::UnboundedReceiver, task::JoinHandle};
 
 pub type KResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 pub struct Kashimo {
-	pub client: Client,
-	pub account: Account,
-	pub event_handler: Mutex<Option<JoinHandle<()>>>
+	client: Client,
+	account: Account,
 }
 
 impl Kashimo {
-	pub async fn connect(host: &str) -> KResult<Arc<Kashimo>> {
+	pub async fn connect(host: &str) -> KResult<(Arc<Kashimo>, UnboundedReceiver<Event>)> {
 		let account = Account::offline("Kashimo");
-		let (client, mut receiver) = Client::join(&account, host).await?;
+		let (client, receiver) = Client::join(&account, host).await?;
 		let kashimo = Arc::new(
 			Kashimo {
-				client: client,
-				account: account,
-				event_handler: Mutex::new(None)
+				client,
+				account
 			}
 		);
-		let kashimo_clone = (&kashimo).clone();
-		*(kashimo.event_handler.lock().await) = Some(
-			tokio::spawn(async move {
-				while let Some(event) = receiver.recv().await {
-					kashimo_clone.handle_event(event).await;
-				}
-			}
-		));
-		Ok(kashimo)
+		Ok((kashimo, receiver))
 	}
 
-	pub async fn wait_for_events(&self) -> KResult<()> {
-		let mut lock = self.event_handler.lock().await;
-		match lock.as_mut() {
-			Some(handle) => {
-				handle.await?;
-				*lock = None;
+	pub fn wait_for_events(bot: &Arc<Self>, mut receiver: UnboundedReceiver<Event>) -> JoinHandle<()> {
+		let bot = bot.clone();
+		tokio::spawn(async move {
+			while let Some(event) = receiver.recv().await {
+				bot.handle_event(event).await;
 			}
-			None => {}
-		}
-		Ok(())
+		})
 	}
 
 	async fn handle_event(&self, event: Event) -> () {
 		match event {
 			Event::Chat(msg) => {
 				println!("{}", msg.message().to_ansi());
+			}
+			Event::Packet(packet) => {
+				match packet.as_ref() {
+					ClientboundGamePacket::BlockUpdate(event) => {
+						println!("BlockUpdate ({}, {}, {})", event.pos.x, event.pos.y, event.pos.z);
+					}
+					_ => {}
+				}
 			}
 			_ => {}
 		}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod kashimo;
 
 #[tokio::main]
 async fn main() -> KResult<()> {
-	let bot = Kashimo::connect("localhost:44377").await?;
-	bot.wait_for_events().await?;
+	let (bot, receiver) = Kashimo::connect("localhost:44377").await?;
+	Kashimo::wait_for_events(&bot, receiver).await?;
 	Ok(())
 }
